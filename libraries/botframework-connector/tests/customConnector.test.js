bab030a12830e0671d53a930de9e2b17c399d59e
Santiago Grangetto <santiagograngetto@gmail.com>
2019-09-26 16:23:15 -0300

    Update mocking in get token tests
    - configured variables to be used in from .env file and added the required variables in the file
    - refactor to save in record mode only the calls needed to mock
    - refactor to get the bearer token from recorded file in lockdown mode

--- libraries/botframework-connector/tests/customConnector.test.js
+++ libraries/botframework-connector/tests/customConnector.test.js
@@ -1,25 +1,63 @@
-require('dotenv').config({ path: 'tests/.env' });
+require('dotenv').config({path: __dirname + '/.env'});
+const fs = require('fs-extra');
+const path = require('path');
 const assert = require('assert');
 const customBotframeworkConnector = require('../lib');
 const { MockMode, usingNock } = require('./mockHelper');
 const nock = require('nock');
 
-const userId = 'UK8CH2281:TKGSUQHQE';
 const baseUri = 'https://api.botframework.com';
-const authAppId = '2cd87869-38a0-4182-9251-d056e8f0ac24';
-const authAppPassword = '2.30Vs3VQLKt974F';
 const userAgent = 'Microsoft-BotFramework/3.1 BotBuilder/4.1.6 (Node.js,Version=v12.6.0; Windows_NT 10.0.17763; x64)';
 
-// Use 'MockMode.lockdown' to use recorded results or 'MockMode.record' to record new results
-const mode = MockMode.lockdown;
+/*
+** To use this test select one of these modes:
+** -MockMode.lockdown to use the test with the mocked files.
+** -MockMode.record to use the test normal and record new mock files.
+** -MockMode.wild to use the test without mocks and without recording.
+*/
+const mode = MockMode.wild;
 
-// Do not upload the following vars
-const trueUserId = process.env['TRUE_USER_ID'];
-const wrongUserId = process.env['WRONG_USER_ID'];
+// Set up this variables in your .env file
+const userId = process.env['USER_ID'];
 const appId = process.env['APP_ID'];
 const appPassword = process.env['APP_PASSWORD'];
+const connectionName = process.env['CONNECTION_NAME'];
 
-describe('CustomConnector getToken', function() {
+let customCredentials = customBotframeworkConnector.CustomMicrosoftAppCredentials;
+let customClient = customBotframeworkConnector.CustomTokenApiClient;
+let options;
+
+function setHeaderForTest(test) {
+    if(mode === MockMode.lockdown) {
+        try {
+            const fileLocation = `${ path.join(__dirname, 'TestData', test.parent.title) }\\${ test.title.replace(/ /g, '_') }.json`;
+            let file = fs.readFileSync(fileLocation);
+            let jsonFile = JSON.parse(file);
+            let authToken = jsonFile[0].reqheaders.authorization[0];
+            options = {
+                channelId: 'emulator',
+                headers: { 'authorization': [authToken] }
+            };
+        } catch(e) {
+            throw new Error('No recorded object has been provided for this test.');
+        }        
+    }
+}
+
+describe('CustomConnector getToken', async function() {
+
+    before(async function() {
+        customCredentials = new customBotframeworkConnector.CustomMicrosoftAppCredentials(appId, appPassword); 
+        customClient = new customBotframeworkConnector.CustomTokenApiClient(customCredentials, { baseUri: baseUri, userAgent: userAgent } );
+        bearerToken = await customCredentials.getToken(true);
+        authToken = `Bearer ${ bearerToken }`;
+        const custHeader = { 'authorization': [authToken] };
+
+        options = {
+            channelId: 'emulator',
+            headers: custHeader
+        };      
+    });
 
     beforeEach(function(done) {
         nock.cleanAll();
@@ -27,71 +65,42 @@ describe('CustomConnector getToken', function() {
         done();
     });
 
-    xit('should throw expected 401 error message.', function() {
+    it('should throw expected 401 error message.', function() {
 
         return usingNock(this.test, mode)
-            .then(({ nockDone, context }) => {
-                const customCredentials = new customBotframeworkConnector.CustomMicrosoftAppCredentials(authAppId, authAppPassword); 
-                const customClient = new customBotframeworkConnector.CustomTokenApiClient(customCredentials, { baseUri: baseUri, userAgent: userAgent } );
-                
-                const custHeader = mode === MockMode.lockdown ? { 'authorization': ['Bearer fakeToken'] }: undefined;
+            .then(({ nockDone }) => {
 
                 let options = {
                     channelId: 'emulator',
-                    headers: custHeader
+                    headers: { 'authorization': ['Bearer fakeToken'] }
                 };
 
-                return (customClient.userToken.getToken(userId, 'authname', options))
+                return (customClient.userToken.getToken('04cb9d92-5faf-446e-8393-74720c952e22', connectionName, options))
                     .then((response) => {
                         assert(response._response.status === 401);
                     })
-                    .catch((onReject) => {
-                        nockDone(onReject.message);
-                    })
                     .then(nockDone);
             });    
     });
 
-    xit('should throw expected 404 error message.', function() {
+    it('should throw expected 404 error message.', function() {
         this.timeout(100000);
-        
+        setHeaderForTest(this.test);
         return usingNock(this.test, mode)
-            .then(( {nockDone, context }) => {
-                const customCredentials = new customBotframeworkConnector.CustomMicrosoftAppCredentials(appId, appPassword); 
-                const customClient = new customBotframeworkConnector.CustomTokenApiClient(customCredentials, { baseUri: baseUri, userAgent: userAgent } );
-                
-                const custHeader = mode === MockMode.lockdown ? { 'authorization': ['Bearer customToken'] }: undefined;
-
-                let options = {
-                    channelId: 'emulator',
-                    headers: custHeader
-                };
-
-                return (customClient.userToken.getToken(wrongUserId, 'authname', options))
+            .then(( {nockDone }) => {                
+                return (customClient.userToken.getToken('04cb9d92-5faf-446e-8393-74720c952e22', connectionName, options))
                     .then((response) => {
                         assert(response._response.status === 404);                        
-                    }).catch((onReject) => {
-                        assert(false, onReject.message);
                     })
                     .then(nockDone);
             });    
     });
 
-    it('should throw expected 200 message.', function() {
-        
+    it('should throw expected 200 message.', async function() {
+        setHeaderForTest(this.test);        
         return usingNock(this.test, mode)
-            .then(({ nockDone, context }) => {
-                const customCredentials = new customBotframeworkConnector.CustomMicrosoftAppCredentials(appId, appPassword); 
-                const customClient = new customBotframeworkConnector.CustomTokenApiClient(customCredentials, { baseUri: baseUri, userAgent: userAgent } );
-
-                const custHeader = mode === MockMode.lockdown ? { 'authorization': ['Bearer customToken'] }: undefined;
-
-                let options = {
-                    channelId: 'emulator',
-                    headers: custHeader
-                };
-
-                return (customClient.userToken.getToken(trueUserId, 'authname', options))
+            .then(async ({ nockDone }) => {                
+                return (customClient.userToken.getToken(userId, connectionName, options))
                     .then((response) => {
                         assert(response._response.status === 200);
                     })
