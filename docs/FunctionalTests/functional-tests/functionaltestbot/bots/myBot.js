ae63e4145d32aee9dd386a0ef76669b885377d46
Joe <jose.ques@southworks.com>
2019-12-16 20:22:00 -0300

    Format project files and add guidance comments

--- docs/FunctionalTests/functional-tests/functionaltestbot/bots/myBot.js
+++ docs/FunctionalTests/functional-tests/functionaltestbot/bots/myBot.js
@@ -3,18 +3,22 @@
  * Licensed under the MIT License.
  */
 
+// Destructuring Assignments only requires the ActivityHandler class.
 const { ActivityHandler } = require('botbuilder');
 
+// Define our Bot class extending the ActivityHandler class.
 class MyBot extends ActivityHandler {
     constructor(conversationState) {
         super();
+
         this.conversationState = conversationState;
         this.conversationStateAccessor = this.conversationState.createProperty('test');
+        // Register handler that will be executed every time the User sends a message
         this.onMessage(async (context, next) => {
-
+            // Get the conversation context
             var state = await this.conversationStateAccessor.get(context, { count: 0 });
 
-            await context.sendActivity(`you said "${ context.activity.text }" ${ state.count }`);
+            await context.sendActivity(`you said "${context.activity.text}" ${state.count}`);
 
             state.count++;
             await this.conversationState.saveChanges(context, false);
@@ -25,11 +29,11 @@ class MyBot extends ActivityHandler {
             const membersAdded = context.activity.membersAdded;
             for (let cnt = 0; cnt < membersAdded.length; cnt++) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    await context.sendActivity(`welcome ${ membersAdded[cnt].name }`);
+                    await context.sendActivity(`welcome ${membersAdded[cnt].name}`);
                 }
             }
             await next();
-        });        
+        });
     }
 }
 
