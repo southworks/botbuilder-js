daca5681aaba8c2a76624a2e5cb618454ac49258
JhontSouth <jhonatan.sandoval@southworks.com>
2024-06-26 14:35:40 -0500

    add webpack config

--- testing/browser-functional/browser-echo-bot/webpack.config.js
+++ testing/browser-functional/browser-echo-bot/webpack.config.js
@@ -29,6 +29,10 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /node_modules[\\\/](microsoft-cognitiveservices-speech-sdk)[\\\/].*\.(js|ts)$/,
+                use: 'babel-loader'
             }
         ]
     },
@@ -39,14 +43,16 @@ module.exports = {
                 { from: resolve(__dirname, 'index.html'), to: '' }
             ]
         }),
-        // Work around for Buffer is undefined:
-        // https://github.com/webpack/changelog-v5/issues/10
         new webpack.ProvidePlugin({
             Buffer: ['buffer', 'Buffer'],
         }),
         new webpack.ProvidePlugin({
             process: 'process/browser',
         }),
+        new webpack.IgnorePlugin({
+            resourceRegExp: /^\.\/locale$/,
+            contextRegExp: /moment$/
+        })
     ],
     resolve: {
         extensions: ['.css', '.js', '.ts'],
