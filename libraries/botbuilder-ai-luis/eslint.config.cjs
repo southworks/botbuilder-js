615f26921fb8b61dbff44d9bf4861299a667ba1a
Joel Mut <joel.mut@southworks.com>
2024-10-21 13:40:55 -0300

    Fix eslint

--- libraries/botbuilder-ai-luis/eslint.config.cjs
+++ libraries/botbuilder-ai-luis/eslint.config.cjs
@@ -1,10 +1,10 @@
-const js = require("@eslint/js");
-const sharedConfig = require("../../eslint.config.cjs")
+const js = require('@eslint/js');
+const sharedConfig = require('../../eslint.config.cjs');
 
 module.exports = [
     js.configs.recommended,
     ...sharedConfig,
     {
-        files: ["**/*.js, **/*.ts"],
-    }
-]
+        files: ['**/*.js, **/*.ts'],
+    },
+];
