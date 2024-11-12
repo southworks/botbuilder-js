52b50eb85507fd20f8726662909b81939e0b086e
CeciliaAvila <cecilia.avila@southworks.com>
2024-11-12 11:38:54 -0300

    Change templates' version to match libraries

--- generators/generator-botbuilder/generators/app/templates/echo/package.json.ts
+++ generators/generator-botbuilder/generators/app/templates/echo/package.json.ts
@@ -1,6 +1,6 @@
 {
     "name": "<%= botname %>",
-    "version": "1.0.0",
+    "version": "4.1.6",
     "description": "<%= botDescription %>",
     "author": "Generated using Microsoft Bot Builder Yeoman generator v<%= version %>",
     "license": "MIT",
@@ -18,7 +18,7 @@
         "url": "https://github.com"
     },
     "dependencies": {
-        "botbuilder": "~4.23.1",
+        "botbuilder": "4.1.6",
         "dotenv": "~8.2.0",
         "replace": "~1.2.0",
         "restify": "~11.1.0"
